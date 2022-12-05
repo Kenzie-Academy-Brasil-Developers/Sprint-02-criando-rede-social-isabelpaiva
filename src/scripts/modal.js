0da7bd8d0ebef0b4feb55f25b28e1edbf7a441dc
isabelpaiva <isxpaiva@icloud.com>
2022-12-05 14:30:09 -0300

    rede social

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -22,6 +22,15 @@ function renderModal() {
 renderModal()
 
 function createModal(id){
+
+    //USER
+    let divImg = document.createElement('div')
+   let divUser = document.createElement('div')
+   const userImg = document.createElement('img')
+   const userName = document.createElement('h2')
+   const userStack = document.createElement('p')
+
+    //POST
     const modalClose = document.createElement('button')
     const modalContainer = document.createElement('div')
     const modalTitle = document.createElement('h2')
@@ -30,15 +39,35 @@ function createModal(id){
  
 
     let element = {}
+    let user = {}
 
     for (let i =0; i < posts.length; i++){
         if (posts[i].id_post === Number(id)){
             element = posts[i]
         }
+
     }
-    
+
+    for (let i =0; i < users.length; i++){
+      if (users[i].id === Number(element.user)){
+          user = users[i]
+      }
+    }
+
     modalContainer.classList.add('div__modal')
 
+
+    //USER
+    userName.innerText = user.user
+    userName.classList.add('userName')
+
+    userStack.innerText = user.stack
+    userStack.classList.add('userStack')
+
+    userImg.src = user.img
+    userImg.classList.add('userImg')
+
+    //POST
     modalTitle.innerText = element.title
     modalTitle.classList.add('modal__title')
 
@@ -48,7 +77,11 @@ function createModal(id){
     modalClose.innerText = 'X'
     modalClose.classList.add('modal__btn')
 
-    modalContainer.append(modalTitle, modalDescription, modalClose)
+    divImg.appendChild(userImg)
+    divUser.append(userName, userStack)
+   
+
+    modalContainer.append(divImg,divUser, modalTitle, modalDescription, modalClose, )
 
     return modalContainer
 }
